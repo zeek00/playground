e9ca319231ee4a8e10414a489e2a990a1d5008c5
zeek00 <44764168+zeek00@users.noreply.github.com>
2024-09-16 20:18:14 +0100

    production environment setup

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Output directory for production build
+    sourcemap: false, // Disable source maps for production (optional)
+    minify: 'esbuild', // Minify the output
+  },
   preview: {
     host: '0.0.0.0',           // Expose the server on all network interfaces
-    port: process.env.PORT || 4173,  // Use Railway's dynamic port or fallback to
+    port: process.env.PORT,  // Use Railway's dynamic port or fallback to
   }
 });
