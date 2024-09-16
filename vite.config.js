e5a9e04aa3b0b6346e1cb2d88cc20f6e40f36e6c
zeek00 <44764168+zeek00@users.noreply.github.com>
2024-09-16 16:53:28 +0100

    vite config file updated for previewing file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    host: '0.0.0.0',           // Expose the server on all network interfaces
+    port: process.env.PORT || 4173,  // Use Railway's dynamic port or fallback to
+  }
+});
